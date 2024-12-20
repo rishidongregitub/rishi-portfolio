5b88cbb5c6c7db33729913c7d32519305ab50b26
DESKTOP-368HLSI\lenovo <rishidongre519@gmail.com>
2024-12-20 10:13:10 +0530

    porject creation with shadcn ui and some basic stuff with default font and bg color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,41 @@
+import { DEFAULT_RUNTIME_WEBPACK } from 'next/dist/shared/lib/constants';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+	container : {
+		center:true,
+		padding: "15px",
+	},
+	screen : {
+		sm:"648px",
+		md:"768px",
+		lg:"968px",
+		xl:"1200px"
+	},
+	fontFamily:{
+		primary : "Arial, Helvetica, sans-serif"
+	},
+  	extend: {
+  		colors: {
+  			primary: "#1c1c22",
+  			accent: {
+				DEFAULT : "#00ff99",
+				hover : "#00e187"
+			},
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };

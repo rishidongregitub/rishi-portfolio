467b1487cca62e4841213732003fd6e14478ab2a
DESKTOP-368HLSI\lenovo <rishidongre519@gmail.com>
2024-12-23 21:17:16 +0530

    Photo for hero section

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Photo from "@/components/Photo";
 import Social from "@/components/Social";
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
@@ -8,7 +9,7 @@ const Home = () => {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/*Text*/}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span>Software Developer</span>
             <h1 className="h1 mb-6">
               Hello I'm <br />
@@ -32,7 +33,9 @@ const Home = () => {
             </div>
           </div>
           {/*Photo*/}
-          <div className="">Photo</div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo/>
+          </div>
         </div>
       </div>
     </section>
